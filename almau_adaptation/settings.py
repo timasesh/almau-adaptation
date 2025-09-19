8f5c491df8e08cfc43fc99892947fee62cee2afd
Kekhayev Rodion <Kekhayev.r@mail.ru>
2025-09-19 23:20:53 +0500

    deploy5

--- almau_adaptation/settings.py
+++ almau_adaptation/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = 'django-insecure-&2lc)agzzmh11&t^6*%!cga7g2kj-ek((@6%()904rm&)m3$b7
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', '192.168.45.232', 'adaptation.almau.edu.kz']
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1",
