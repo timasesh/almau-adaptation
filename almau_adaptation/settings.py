66d616002894a11f6c2a2b6ecb9ba9a3291e6d9d
Kekhayev Rodion <Kekhayev.r@mail.ru>
2025-09-19 23:29:01 +0500

    deploy5

--- almau_adaptation/settings.py
+++ almau_adaptation/settings.py
@@ -40,6 +40,11 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost",
     "http://testserver",
 ]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
