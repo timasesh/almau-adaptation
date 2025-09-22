6dadfd20a41a4f5934a90b7dc0e497ec6e30544d
Kekhayev Rodion <Kekhayev.r@mail.ru>
2025-09-22 12:00:54 +0500

    lang fix

--- almau_adaptation/settings.py
+++ almau_adaptation/settings.py
@@ -66,12 +66,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
